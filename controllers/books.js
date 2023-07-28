8a3173104d9c18c73d9cb67955e88c4a1690ffff
Joshua <joshua.soave@gmail.com>
2023-07-28 19:46:35 -0400

    remove mongo and mongoose

---
+++ controllers/books.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+var bodyParser = require("body-parser");
+
+require("dotenv").config();
+const apiKey = process.env.REACT_APP_APIKEY;
+
+router.get("/currentFiction", (req, res) => {
+  axios
+    .get(
+      `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKey}`
+    )
+    .then((response) => {
+      res.send(response.data);
+    });
+});
+
+module.exports = router;
